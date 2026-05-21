cc1272f0550a96c270ff8acd6007b42f7c8737c2
Christoph Gleinser <gc0453@mci4me.at>
2026-05-21 11:50:39 +0200

    Initial Commit

--- pandas_df.py
+++ pandas_df.py
@@ -1,10 +1,5 @@
 import pandas as pd
 
-<<<<<<< HEAD
-directory = "data/"
-file_name_csv = "activity.csv"
-df_csv = pd.read_csv(directory + file_name_csv, sep=',',header=0)
-=======
 def df_csv():
     directory = "data/" #Speicherort
     file_name_csv = "activity.csv" #Dateiname der csv-Datei
@@ -13,4 +8,3 @@ def df_csv():
     df_clean = df.dropna(subset=["HeartRate"]) #Alle None aus HeartRate entfernen
     return df_clean #Die "gereinigten" Daten werden zurückgegeben
 
->>>>>>> 0a0eb114fcf663cf32574fbbd3e980d918d428c1
