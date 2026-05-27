31780c03f8dee85353f30e6475736fee6db5245b
Luggi003 <al3320@mci4me.at>
2026-05-27 11:53:27 +0200

    ergänzung balkendiagramm

--- make_plot.py
+++ make_plot.py
@@ -1,6 +1,8 @@
 import plotly.express as plt
 import plotly.graph_objects as go
+from plotly.io import show
 from plotly.subplots import make_subplots
+from analyse import HF_Zonen
 from pandas_df import df_csv #kann später gelöscht werden, wenn die Daten direkt übergeben werden können
 
 #plot, mit 2 y-Achsen, Leistung und Herzfrequenz
@@ -36,8 +38,17 @@ def plot_line(data):
     
     return fig'''
 
-#später löschen, nur zum Testen
-'''
-fig2 = plot_line(df_csv())
-fig2.show()
-'''
+HF_Zone1, HF_Zone2, HF_Zone3, HF_Zone4, HF_Zone5, HF_Zone1_time, HF_Zone2_time, HF_Zone3_time, HF_Zone4_time, HF_Zone5_time = HF_Zonen(df_csv())
+
+def bar(HF_Zone1, HF_Zone2, HF_Zone3, HF_Zone4, HF_Zone5):
+    fig_bar = go.Figure()
+    # Herzfrequenz
+    fig_bar.add_trace(go.Bar (y=[len(HF_Zone1)], name='HeartRate_Zone1'))
+    fig_bar.add_trace(go.Bar (y=[len(HF_Zone2)], name='HeartRate_Zone2'))
+    fig_bar.add_trace(go.Bar (y=[len(HF_Zone3)], name='HeartRate_Zone3'))
+    fig_bar.add_trace(go.Bar (y=[len(HF_Zone4)], name='HeartRate_Zone4'))
+    fig_bar.add_trace(go.Bar (y=[len(HF_Zone5)], name='HeartRate_Zone5'))
+
+
+    fig_bar.update_layout( title='Zeit in den 5 verschiedenen Herzfrequenz-Zonen', xaxis_title='Zonen', yaxis_title='Zeit', barmode='group')
+    return fig_bar
