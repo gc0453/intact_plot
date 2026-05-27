dbd374af79238cf79e091d33041ac62cb9bd5bb7
Christoph Gleinser <gc0453@mci4me.at>
2026-05-27 15:58:48 +0200

    Dokumentation README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+#Programminfo:
+
+Das Programm ist die zweite Aufgabe für die Lehrveranstaltung Programmierübung durchgeführt von Auer Lukas und Gleinser Christoph.
+
+#Vorgabe:
+
+Aufgabe ist es die Datei activity.csv einzulesen und die Leistungswerte bzw. Herzfrequenzwerte über streamlit grafisch sowie als Zahlenwerte auszugeben. Um die Aufgabe umzusetzen, war wichtigen die gegeben Daten zu "reinigen" (leere Werte bzw. Werte mit 0 entfernen). Gefragt waren außerdem die Durchschnittswerte.
+
+#Installationsanleitung:
+
+Bevor das Programm verwendet werden kann, muss das Repository auf Ihr Gerät geklont ( Befehl: git clone https://github.com/gc0453/intact_plot.git) und mit PDM installiert werden ( Befehl: pdm install ). Das Programm kann anschließend mit dem folgenden Befehl ausgeführt werden: pdm run streamlit run main.py 
--- main.py
+++ main.py
@@ -4,32 +4,7 @@ from analyse import HF_Zonen, Maximum, Umrechnen, HF_Zonen_zeit, avg_power, durc
 from pandas_df import df_csv
 from make_plot import bar, plot_line
 
-tab1, tab2 = st.tabs(["EKG-Data", "Power-Data"])
 
-'''
-# Session State wird leer angelegt, solange er noch nicht existiert
-if 'current_user' not in st.session_state:
-    st.session_state.current_user = 'None'
-
-# Dieses Mal speichern wir die Auswahl als Session State
-st.session_state.current_user = st.selectbox(
-    'EKG',
-    options = person_names, key="sbVersuchsperson")
-
-st.write("Der Name ist: ", st.session_state.current_user) 
-
-if 'picture_path' not in st.session_state:
-    st.session_state.picture_path = 'data/pictures/none.jpg'
-
-if st.session_state.current_user in person_names:
-    st.session_state.picture_path = read_data.find_person_data_by_name(st.session_state.current_user)['picture_path']
-
-if 'birth_date' not in st.session_state:
-    st.session_state.birth_date = '1.1.1990'
-
-image = Image.open(st.session_state.picture_path)
-st.image(image, caption=st.session_state.current_user)
-'''
 # Wo startet sie Zeitreihe
 # Wo endet sich
 # Was ist die Maximale und Minimale Spannung
